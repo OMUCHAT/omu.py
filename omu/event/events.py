12db62af6773d90f0c7f832cb354908bcd0c618f
am230 <111672334+am230@users.noreply.github.com>
2023-12-08 13:48:28 +0900

    Rename events: Connect, Ready -> connect, ready

--- omu/event/events.py
+++ omu/event/events.py
@@ -2,17 +2,14 @@ from omu.event.event import BuiltinEventType
 from omu.extension.server.model.app import App, AppJson
 from omu.interface import Serializer
 
-Connect = BuiltinEventType[App, AppJson](
-    "Connect",
-    Serializer.model(App.from_json),
-)
-
-Ready = BuiltinEventType[None, None](
-    "Ready",
-    Serializer.noop(),
-)
-
 
 class EVENTS:
-    Connect = Connect
-    Ready = Ready
+    Connect = BuiltinEventType[App, AppJson](
+        "connect",
+        Serializer.model(App.from_json),
+    )
+
+    Ready = BuiltinEventType[None, None](
+        "ready",
+        Serializer.noop(),
+    )
